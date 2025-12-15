32f5d4ce48eda0a709b489e1bc32d75cb738225f
caio-venancio <caio.venancio784@gmail.com>
2025-12-14 22:15:46 -0300

    refactor: fazer em classes

--- frontend2/src/game/main.ts
+++ frontend2/src/game/main.ts
@@ -1,14 +1,7 @@
 import Phaser from "phaser";
-import { LocalService } from "../services/local-service";
+import { GameScene } from "./scenes/GameScene"
 
-/**
- * Tipos auxiliares
- */
-type PlayerSpriteMap = Record<string, Phaser.GameObjects.Rectangle>;
 
-let service: LocalService;
-let players: PlayerSpriteMap = {};
-let cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
 
 /**
  * Configuração do Phaser
@@ -18,79 +11,7 @@ const config: Phaser.Types.Core.GameConfig = {
   parent: "phaser-example",
   width: 800,
   height: 600,
-  scene: {
-    preload,
-    create,
-    update
-  }
+  scene: [GameScene]
 };
 
 new Phaser.Game(config);
-
-/**
- * Phaser lifecycle
- */
-function preload(this: Phaser.Scene) {
-  // carregar assets aqui, se precisar
-}
-
-function create(this: Phaser.Scene) {
-  // 1) Instancia e conecta no serviço local
-  service = new LocalService();
-  service.connect();
-
-  // 2) Captura teclado
-  cursors = this.input.keyboard!.createCursorKeys();
-
-  // 3) Cleanup ao sair da cena
-  this.events.once(Phaser.Scenes.Events.SHUTDOWN, () => {
-    service.disconnect();
-  });
-}
-
-function update(this: Phaser.Scene, _time: number, delta: number) {
-  if (!service) return;
-
-  // 1) Envia input do jogador local
-  service.handleInput(
-    cursors.up?.isDown ?? false,
-    cursors.right?.isDown ?? false,
-    cursors.down?.isDown ?? false,
-    cursors.left?.isDown ?? false
-  );
-
-  // 2) Atualiza a simulação do "servidor"
-  service.update(delta);
-
-  // 3) Sincroniza os jogadores na tela
-  syncPlayers(this);
-}
-
-/**
- * Sincroniza sprites com o estado vindo do Service
- */
-function syncPlayers(scene: Phaser.Scene) {
-  const list = service.getPlayers();
-  const seenIds = new Set<string>();
-
-  list.forEach((p) => {
-    seenIds.add(p.id);
-
-    // cria sprite se não existir
-    if (!players[p.id]) {
-      players[p.id] = scene.add.rectangle(p.x, p.y, 50, 50, 0xffffff);
-    }
-
-    // atualiza posição
-    players[p.id].x = p.x;
-    players[p.id].y = p.y;
-  });
-
-  // remove jogadores que saíram
-  Object.keys(players).forEach((id) => {
-    if (!seenIds.has(id)) {
-      players[id].destroy();
-      delete players[id];
-    }
-  });
-}
---
+++ frontend2/src/game/scenes/GameScene.ts
@@ -0,0 +1,61 @@
+import Phaser from "phaser";
+import { LocalService } from "../../services/local-service";
+
+type PlayerSpriteMap = Record<string, Phaser.GameObjects.Rectangle>;
+
+export class GameScene extends Phaser.Scene {
+  private service!: LocalService;
+  private players: PlayerSpriteMap = {};
+  private cursors!: Phaser.Types.Input.Keyboard.CursorKeys;
+
+  constructor() {
+    super("GameScene");
+  }
+
+  create() {
+    this.service = new LocalService();
+    this.service.connect();
+
+    this.cursors = this.input.keyboard!.createCursorKeys();
+
+    this.events.once(Phaser.Scenes.Events.SHUTDOWN, () => {
+      this.service.disconnect();
+    });
+  }
+
+  update(_time: number, delta: number) {
+    this.service.handleInput(
+      this.cursors.up?.isDown ?? false,
+      this.cursors.right?.isDown ?? false,
+      this.cursors.down?.isDown ?? false,
+      this.cursors.left?.isDown ?? false
+    );
+
+    this.service.update(delta);
+
+    this.syncPlayers();
+  }
+
+  private syncPlayers() {
+    const list = this.service.getPlayers();
+    const seen = new Set<string>();
+
+    for (const p of list) {
+      seen.add(p.id);
+
+      if (!this.players[p.id]) {
+        this.players[p.id] = this.add.rectangle(p.x, p.y, 50, 50, 0xffffff);
+      }
+
+      this.players[p.id].x = p.x;
+      this.players[p.id].y = p.y;
+    }
+
+    for (const id of Object.keys(this.players)) {
+      if (!seen.has(id)) {
+        this.players[id].destroy();
+        delete this.players[id];
+      }
+    }
+  }
+}
